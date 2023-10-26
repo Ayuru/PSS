7ad54433d5225c59c0dcc61c34dbb468e0396839
Ayu <ayuru.gi@gmail.com>
2023-10-26 22:59:56 +0200

    crash fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,12 @@ public class Main {
         boolean run = true;
 
         while (run) {
-            run = false;
             GameBoard game = new GameBoard();
             try {
                 Pair userData = game.intro();
                 run = game.play(userData.getName(), userData.getRounds());
             } catch (Exception e) {
-                System.out.println("Oh no! Something went wrong! Error: " + e);
+                System.out.println("Oh no! Something went wrong! Error: " + e + "\nLets try again!\n\n");
             }
         }
 
